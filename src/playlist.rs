9071def6555d8b4a39f2975f34ee586054c165b1
Nathan Merrill <nathan.d.merrill@gmail.com>
2015-10-25 16:58:35 -0700

    Make playlist module work for multiple songs.

--- src/playlist.rs
+++ src/playlist.rs
@@ -1,11 +1,11 @@
 //  Copyright 2015 Nathanael Merrill
-
+//
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
-
+//
 //    http://www.apache.org/licenses/LICENSE-2.0
-
+//
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,21 @@
 use std::string;
 
 pub struct Playlist {
-    music: string::String,
+    songs: Vec<String>,
+    song_index: i32,
 }
 
 impl Playlist {
-    pub fn new(song: &str) -> Playlist {
-        Playlist{music: song.to_string()}
+    pub fn new(songs: Vec<String>) -> Playlist {
+        Playlist{songs: songs,
+                 song_index: -1}
     }
 
-    pub fn get_next_song(&self) -> &str {
-        &self.music
+    pub fn get_next_song(&mut self) -> Option<&str> {
+        self.song_index += 1;
+        match self.songs.get(self.song_index as usize) {
+            Some(a) => Some(&a),
+            None => None,
+        }
     }
 }
